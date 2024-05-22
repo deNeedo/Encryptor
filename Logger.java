39b36fbb77a4783f0309448dae04e6df46e8c6c3
deNeedo <jkudrys@protonmail.com>
2024-05-22 23:27:51 +0200

    Added own Logger class

---
+++ Logger.java
@@ -0,0 +1,5 @@
+public class Logger
+{
+    public static void info(String text) {System.out.println("[INFO] : " + text);}
+    public static void error(String text) {System.out.println("[ERROR] : " + text);}
+}
