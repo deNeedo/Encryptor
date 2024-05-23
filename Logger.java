eb327b265290a397127ae719a30a6080c391597a
deNeedo <jkudrys@protonmail.com>
2024-05-23 19:26:00 +0200

    Added warning message type to the Logger.

--- Logger.java
+++ Logger.java
@@ -1,5 +1,6 @@
 public class Logger
 {
-    public static void info(String text) {System.out.println("[INFO] : " + text);}
-    public static void error(String text) {System.out.println("[ERROR] : " + text);}
+    public static void info(String text) {System.out.println("   [INFO] : " + text);}
+    public static void warning(String text) {System.out.println("[WARNING] : " + text);}
+    public static void error(String text) {System.out.println("  [ERROR] : " + text);}
 }
