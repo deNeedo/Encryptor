54647eee2968e29b93aad26b50e6401cd020ec13
deNeedo <jkudrys@protonmail.com>
2024-05-26 19:42:01 +0200

    Removed unused imports

--- src/main/java/Encryptor.java
+++ src/main/java/Encryptor.java
@@ -3,11 +3,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Base64;
-import java.util.stream.Collectors;
 
 import javax.crypto.Cipher;
 /* Main class */
